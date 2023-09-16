51eec6a244ab1e9a28ebbfcde87ec0f645f794eb
Zane_Goodman <zgoodman1776@gmail.com>
2023-09-16 19:44:58 -0400

    All but testing completed

--- makeText.js
+++ makeText.js
@@ -1,2 +1,50 @@
 /** Command-line tool to generate Markov text. */
+const fs = require("fs");
+const axios = require("axios");
+const argType = process.argv[2];
+const { MarkovMachine } = require("./markov");
 
+checkIfFileOrURL(argType);
+
+async function checkIfFileOrURL(arg) {
+  try {
+    if (arg === "url") {
+      await printNewTextFromURL(process.argv[3]);
+
+      return;
+    }
+    if (arg === "file") {
+      printNewTextFromFile(process.argv[3]);
+
+      return;
+    }
+  } catch {
+    console.log(err);
+    process.kill(1);
+  }
+}
+
+function printNewTextFromFile(arg) {
+  fs.readFile(arg, "utf8", (err, data) => {
+    if (err) {
+      console.log("ERROR", err);
+      process.kill(1);
+    }
+
+    let sentence = new MarkovMachine(data);
+
+    console.log(sentence.makeText(50));
+  });
+}
+
+async function printNewTextFromURL(url) {
+  try {
+    response = await axios.get(url);
+
+    let sentence = new MarkovMachine(response.data);
+    console.log(sentence.makeText(50));
+  } catch {
+    console.log("ERROR", "Invalid Url");
+    process.kill(1);
+  }
+}
--- markov.js
+++ markov.js
@@ -1,14 +1,12 @@
 /** Textual markov chain generator */
 
-
 class MarkovMachine {
-
   /** build markov machine; read in text.*/
 
   constructor(text) {
-    let words = text.split(/[ \r\n]+/);
-    this.words = words.filter(c => c !== "");
-    this.makeChains();
+    let words = text.split(/[ \r\n . ,]+/);
+    this.words = words.filter((c) => c !== "");
+    this.wordChain = this.makeChains();
   }
 
   /** set markov chains:
@@ -17,13 +15,56 @@ class MarkovMachine {
    *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
 
   makeChains() {
-    // TODO
+    let obj = {};
+    let words = this.words;
+    for (let i = 0; i < words.length; i++) {
+      let key = words[i];
+      let wordToAdd = words[i + 1];
+
+      let checkKeys = Object.keys(obj).some((value) => {
+        return value === key;
+      });
+
+      if (wordToAdd === undefined) {
+        wordToAdd = null;
+      }
+
+      checkKeys === true ? obj[key].push(wordToAdd) : (obj[key] = [wordToAdd]);
+    }
+    return obj;
   }
 
-
-  /** return random text from chains */
+  getRandomIndex(range) {
+    let randomIndex = Math.floor(Math.random() * range);
+    return randomIndex;
+  }
 
   makeText(numWords = 100) {
-    // TODO
+    let word;
+
+    let wordChain = this.wordChain;
+    let keys = Object.keys(this.wordChain);
+
+    let randKeyIndex = this.getRandomIndex(keys.length);
+    let startWord = keys[randKeyIndex];
+    let newSentence = "";
+    newSentence = newSentence + " " + startWord;
+
+    word = startWord;
+    for (let i = 0; i < numWords; i++) {
+      word =
+        wordChain[word][
+          this.getRandomIndex(Object.values(wordChain[word]).length)
+        ];
+      if (word !== null) {
+        newSentence = newSentence + " " + word;
+      } else {
+        console.log(newSentence);
+        process.exit(1);
+      }
+    }
+    return newSentence;
   }
 }
+
+module.exports = { MarkovMachine };
